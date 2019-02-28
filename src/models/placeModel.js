83d105cf60059a13988fe1ff14e4c9a9afa2aff9
Muhun Kim <iam@muhun.kim>
2019-03-01 00:07:00 +0900

    Tasks screen styling done
    + coin screen positions edit

--- src/models/placeModel.js
+++ src/models/placeModel.js
@@ -1,145 +1,55 @@
+// import list from './db/places.json'
+
 export default {
 	data: [{
-			name: 'KimyoungGap',
-			img: 'KimyoungGap.png',
-			items: [{
-					name: 'KimyoungGapOne',
-					img: 'profile.jpg',
-					detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.',
-					tag: '#want #go #home',
-					completed: false
-				},
-				{
-					name: 'TKimyoungGapwo',
-					img: 'profile.jpg',
-					detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.',
-					tag: '#want #go #home',
-					completed: false
-				},
-				{
-					name: 'ThreKimyoungGape',
-					img: 'profile.jpg',
-					detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.',
-					tag: '#want #go #home',
-					completed: false
-				},
-				{
-					name: 'FKimyoungGapour',
-					img: 'profile.jpg',
-					detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.',
-					tag: '#want #go #home',
-					completed: false
-				}
-			],
-			coin: 0,
-			visitedDate: 'Day 20 Fri'
-		},
-		{
-			name: 'Brick Campus',
-			img: 'BrickCampus.jpeg',
-			items: [{
-					name: 'OBrick Campusne',
-					img: 'profile.jpg',
-					detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.',
-					tag: '#want #go #home',
-					completed: false
-				},
-				{
-					name: 'Two Brick Campus',
-					img: 'profile.jpg',
-					detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.',
-					tag: '#want #go #home',
-					completed: false
-				},
-				{
-					name: 'ThBrick Campusree',
-					img: 'profile.jpg',
-					detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.',
-					tag: '#want #go #home',
-					completed: false
-				},
-				{
-					name: 'Brick CampusFour',
-					img: 'profile.jpg',
-					detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.',
-					tag: '#want #go #home',
-					completed: false
-				}
-			],
-			coin: 0,
-			visitedDate: 'Day 15 Wed'
-		},
-		{
-			name: 'Jayeonsarang',
-			img: 'Jayeonsarang.jpeg',
-			items: [{
-					name: 'OneJayeonsarang',
-					img: 'profile.jpg',
-					detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.',
-					tag: '#want #go #home',
-					completed: false
-				},
-				{
-					name: 'TwJayeonsarango',
-					img: 'profile.jpg',
-					detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.',
-					tag: '#want #go #home',
-					completed: false
-				},
-				{
-					name: 'JayeonsarangThree',
-					img: 'profile.jpg',
-					detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.',
-					tag: '#want #go #home',
-					completed: false
-				},
-				{
-					name: 'FoJayeonsarangur',
-					img: 'profile.jpg',
-					detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.',
-					tag: '#want #go #home',
-					completed: false
-				}
-			],
-			coin: 0,
-			visitedDate: 'Day 12 Fri'
-		},
-		{
-			name: 'Gallery nori',
-			img: 'nori.jpeg',
-			items: [{
-					name: 'nori One',
-					img: 'profile.jpg',
-					detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.',
-					tag: '#want #go #home',
-					completed: false
-				},
-				{
-					name: 'Two Gallery',
-					img: 'profile.jpg',
-					detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.',
-					tag: '#want #go #home',
-					completed: false
-				},
-				{
-					name: 'nory Three',
-					img: 'profile.jpg',
-					detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.',
-					tag: '#want #go #home',
-					completed: false
-				},
-				{
-					name: 'Four Gallery',
-					img: 'profile.jpg',
-					detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.',
-					tag: '#want #go #home',
-					completed: false
-				}
-			],
-			coin: 0,
-			visitedDate: 'Day 10 Mon'
-		}
-	],
+		name: "KimyoungGap",
+		img: "KimyoungGap.png",
+		items: [{
+			name: "용눈이 오름",
+			img: "e-audio.jpg",
+			detail: "20년 전 오름은 사람들의 관심을 끌지 못했다. 매스컴이나 관광안내서에서도 오른을 비중 있게 다루지 않았다. 주로 백록담 주변의 고산지대를 집중적으로 부각시켰고, 그렇게 모두들 한라산 하면 백록담을 연상했다.",
+			tags: ["용눈이_오름", "바람", "오름"],
+			completed: false
+		}],
+		coin: 0,
+		visitedDate: "Day 20 Fri"
+	}, {
+		name: "Brick Campus",
+		img: "BrickCampus.jpeg",
+		items: [{
+			name: "Brick Campus",
+			img: "profile.jpg",
+			detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.",
+			tags: ["want", "go", "home"],
+			completed: false
+		}],
+		coin: 0,
+		visitedDate: "Day 15 Wed"
+	}, {
+		name: "Jayeonsarang",
+		img: "Jayeonsarang.jpeg",
+		items: [{
+			name: "Jayeonsarang",
+			img: "profile.jpg",
+			detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.",
+			tags: ["want", "go", "home"],
+			completed: false
+		}],
+		coin: 0,
+		visitedDate: "Day 12 Fri"
+	}, {
+		name: "Gallery nori",
+		img: "nori.jpeg",
+		items: [{
+			name: "nori One",
+			img: "profile.jpg",
+			detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis orci, pellentesque at venenatis a, mattis et neque. Aliquam interdum.",
+			tags: ["want", "go", "home"],
+			completed: false
+		}],
+		coin: 0,
+		visitedDate: "Day 10 Mon"
+	}],
 	list() {
 		return Promise.resolve(this.data)
 	},
